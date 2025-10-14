daedbb078af2592c066bbb19ffa0c3b39eba8d47
Knorrrr <99851410+knorrrr@users.noreply.github.com>
2025-10-14 14:26:57 +0900

    chore: delete username from docker image (#132)

--- docker_run.sh
+++ docker_run.sh
@@ -42,4 +42,4 @@ LOG_FILE="$LOG_DIR/docker_run.log"
 echo "A rocker run log is stored at : file://$LOG_FILE"
 
 # shellcheck disable=SC2086
-rocker ${opts} --x11 --devices /dev/dri --user --net host --privileged --name "aichallenge-2025-$(date "+%Y-%m-%d-%H-%M-%S")" --volume ${volume} -- "aichallenge-2025-${target}-${USER}" 2>&1 | tee "$LOG_FILE"
+rocker ${opts} --x11 --devices /dev/dri --user --net host --privileged --name "aichallenge-2025-$(date "+%Y-%m-%d-%H-%M-%S")" --volume ${volume} -- "aichallenge-2025-${target}" 2>&1 | tee "$LOG_FILE"
